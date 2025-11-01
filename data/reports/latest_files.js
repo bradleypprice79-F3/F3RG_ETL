2e32dc039707698c3728b310b54b92fd48bf8c1a
bradleypprice79-F3 <bradley.p.price79@gmail.com>
2025-11-01 17:09:58 +0000

    added missing archive_folder

--- data/reports/latest_files.js
+++ data/reports/latest_files.js
@@ -1,6 +1,6 @@
 const latestFiles = {
-  individual: "individual_scores_20251029_1526.csv",
-  team: "team_scores_20251029_1526.csv",
-  lone_pax: "lone_pax_report_20251029_1526.csv",
-  current_timestamp: "2025-10-29 15:26"
+  individual: "individual_scores_20251101_1202.csv",
+  team: "team_scores_20251101_1202.csv",
+  lone_pax: "lone_pax_report_20251101_1202.csv",
+  current_timestamp: "2025-11-01 12:02"
 };
